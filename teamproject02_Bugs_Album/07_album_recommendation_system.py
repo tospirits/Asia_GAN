c583af560b8ae5dabdd31c16b7bad2e61feb1c8d
tospirits <tospirits@gmail.com>
2021-07-05 18:19:32 +0900

    앨범 추천 시스템 1 완료. 모델 생성 이후 추천 시스템 2 코드 작성 필요

--- teamproject02_Bugs_Album/07_album_recommendation_system.py
+++ teamproject02_Bugs_Album/07_album_recommendation_system.py
@@ -13,12 +13,12 @@ with open('./models/tfidf.pickle', 'rb') as f:
 def getRecommendation(cosine_sim):
     simScore = list(enumerate(cosine_sim[0]))
     simScore = sorted(simScore, key=lambda x:x[1], reverse=True)
-    simScore = simScore[0:11]
+    simScore = simScore[1:11]
     albumidx = [i[0] for i in simScore]
     recAlbumList = df.iloc[albumidx]
     return recAlbumList
 
-album_idx = df[df['album titles']=='WE'].index[0]
+album_idx = df[df['album titles']=='Red Diary Page.1'].index[0]
 
 cosine_sim = linear_kernel(Tfidf_matrix[album_idx], Tfidf_matrix)
 
