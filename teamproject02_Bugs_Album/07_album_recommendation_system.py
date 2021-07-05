287088abcff4310f63d49b990f2045229c45212e
tospirits <tospirits@gmail.com>
2021-07-05 17:36:03 +0900

    앨범 추천 시스템 1 완료. 모델 생성 이후 추천 시스템 2 코드 작성 필요

---
+++ teamproject02_Bugs_Album/07_album_recommendation_system.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from scipy.io import mmread
+import pickle
+from gensim.models import Word2Vec
+
+df = pd.read_csv('./crawling/album_reviews.csv', index_col=0)
+
+Tfidf_matrix = mmread('./models/tfidf_album_review.mtx').tocsr()
+with open('./models/tfidf.pickle', 'rb') as f:
+    Tfidf = pickle.load(f)
+
+def getRecommendation(cosine_sim):
+    simScore = list(enumerate(cosine_sim[0]))
+    simScore = sorted(simScore, key=lambda x:x[1], reverse=True)
+    simScore = simScore[0:11]
+    albumidx = [i[0] for i in simScore]
+    recAlbumList = df.iloc[albumidx]
+    return recAlbumList
+
+album_idx = df[df['album titles']=='WE'].index[0]
+
+cosine_sim = linear_kernel(Tfidf_matrix[album_idx], Tfidf_matrix)
+
+recommendation = getRecommendation(cosine_sim)
+print(recommendation.iloc[:, 0])
