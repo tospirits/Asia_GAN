ec085bf79765189a13874296aae700422b85f6fa
tospirits <tospirits@gmail.com>
2021-07-09 17:30:00 +0900

    pyqt 최종

--- teamproject02_Bugs_Album/qt5_hap.py
+++ teamproject02_Bugs_Album/qt5_hap.py
@@ -2,7 +2,7 @@ import datetime
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtCore import QStringListModel
 from PyQt5.QtCore import Qt
@@ -14,6 +14,7 @@ from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread
 import pickle
 from gensim.models import Word2Vec
+import webbrowser
 
 
 
@@ -37,9 +38,8 @@ class MainPage(QWidget, form_window):
 
         global keywords
         keyword_list = ['클래식', '빌보드', '댄스', '힙합', '알앤비', '영화', '애니메이션', '드라마', '크리스마스',
-                        '겨울', '여름', '여행', '이별', '사랑', '결혼', '우울', '행복']
+                        '겨울', '여름', '여행', '이별', '사랑', '결혼', '우울', '행복', '배경음악', '아이돌', '걸그룹']   # 20개
         random_keyword_list = random.sample(keyword_list, 15)
-        print(random_keyword_list)
 
 
         keywords = [self.keywordButton_01, self.keywordButton_02, self.keywordButton_03, self.keywordButton_04, self.keywordButton_05,
@@ -52,6 +52,104 @@ class MainPage(QWidget, form_window):
         for r in range(15):
             keywords[r].clicked.connect(self.open_recommend_window)
 
+        ### 하단 랜덤 앨범 3개 띄우기 ###
+        df2 = self.df_bugs
+
+        global random_img_list
+
+        img_list = list(df2['album links'])
+        random_img_list = random.sample(img_list, 3)
+
+        random_album_list = []
+        for m in range(3):
+            random_img = self.df_bugs[self.df_bugs['album links'] == random_img_list[m]].iloc[0, 1]
+            random_album_list.append(random_img)
+
+        random_artist_list = []
+        for m in range(3):
+            random_artist = df2[df2['album links']==random_img_list[m]].iloc[0, 2]
+            random_artist_list.append(random_artist)
+
+        albumtitleLabel = [self.albumtitleLabel_01, self.albumtitleLabel_02,self.albumtitleLabel_03]
+        artistLabel = [self.artistLabel_01, self.artistLabel_02,self.artistLabel_03]
+        imgs = [self.albumartLabel_01,self.albumartLabel_02,self.albumartLabel_03]
+        for m in range(3):
+            path = './images/' + str(random_img_list[m]) + '.jpg'
+            pixmap = QPixmap(path)
+            imgs[m].setPixmap(pixmap)
+        for m in range(3):
+            albumtitleLabel[m].setText(random_album_list[m])
+        for m in range(3):
+            artistLabel[m].setText(random_artist_list[m])
+
+        for i in range(3):
+            if len(albumtitleLabel[i].text()) > 24 :
+                albumtitleLabel[i].move(180+i*250, 820)
+                artistLabel[i].move(180 + i * 250, 850)
+
+        for i in range(3):
+            if len(artistLabel[i].text()) > 25 :
+                artistLabel[i].move(180 + i * 250, 860)
+
+        self.linkbuttons = [self.linkButton_1, self.linkButton_2, self.linkButton_3]
+
+        for r in range(3):
+            self.linkbuttons[r].clicked.connect(self.gotoURL)
+
+        self.searchLine.cursorPositionChanged.connect(self.popimage)
+        self.searchDial.sliderReleased.connect(self.activate_dial)
+
+
+    def gotoURL(self):
+        try:
+            main_url = 'https://music.bugs.co.kr/album/'
+
+            self.object_name = self.sender().objectName()
+
+            for i in range(3):
+                if self.object_name == f'linkButton_{i+1}':
+                    imgnum = i
+
+            url = main_url + str(random_img_list[imgnum])
+            webbrowser.open(url)
+        except Exception as e:
+            print(e)
+
+    def popimage(self):
+        try:
+            searchdata = self.searchLine.text()
+            if searchdata.find('(ARTIST)') != -1:       # 앨범이면
+                album_title = searchdata.split('(ARTIST)')[0].rstrip()
+                main_album_art = self.df_bugs[self.df_bugs['album titles'] == album_title].iloc[0, 0]
+                path = './images/' + str(main_album_art) + '.jpg'
+                pixmap = QPixmap(path)
+                self.mainalbumartLabel.setPixmap(pixmap)
+
+            else:       # 아티스트면
+                if len(self.df_bugs[self.df_bugs['artists']==searchdata]) != 0 :
+                    artist = searchdata
+                    main_album_art_list = list(self.df_bugs[self.df_bugs['artists'] == artist]['album links'])
+                    main_album_art = random.sample(main_album_art_list, 1).pop()
+                    path = './images/' + str(main_album_art) + '.jpg'
+                    pixmap = QPixmap(path)
+                    self.mainalbumartLabel.setPixmap(pixmap)
+
+
+        except Exception as e:
+            print(e)
+
+
+
+
+    def activate_dial(self):
+        if self.searchDial.value() >= 50:
+            self.searchDial.setValue(0)         # 다이얼 초기화
+            self.dial_recommend_window()
+            return      # return으로 종료해줘야 반복 실행 안 됨
+        else:           # 없으면 함수 계속 실행돼서 멈춤
+            self.searchDial.setValue(0)  # 다이얼 초기화
+            return
+
 
     def search_album(self):     # 검색창 자동완성 함수
 
@@ -60,7 +158,7 @@ class MainPage(QWidget, form_window):
         df_artist = df.drop_duplicates(['artists'], keep='first', ignore_index=True, inplace=False)
         album_list = list(df_album['album titles'])
 
-        album_list = list(df_album['album titles'])
+        album_list = list(df_album['album titles'] + '      (ARTIST)  ' + df_album['artists'])
         artist_list = list(df_artist['artists'])
 
         model = QStringListModel()
@@ -68,10 +166,15 @@ class MainPage(QWidget, form_window):
 
         completer = QCompleter()
         completer.setModel(model)
+        completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)     # 대소문자 구분 없이
         self.searchLine.setCompleter(completer)
+
+
 
     def open_recommend_window(self):
         global nums, contents
+
+        self.errorLabel.setText('')        # 에러표기창 초기화
 
         self.object_name = self.sender().objectName()
 
@@ -132,7 +235,7 @@ class MainPage(QWidget, form_window):
                 nums.append(num)
                 title = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 1]
                 artist = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 2]
-                contents.append(title + '\n' + artist)
+                contents.append(title + '\n - ' + artist)
 
             print(nums)
             print(contents)
@@ -143,6 +246,79 @@ class MainPage(QWidget, form_window):
 
         print('open window')
         ResultPage(self)
+
+    def dial_recommend_window(self):
+        global nums, contents
+
+        self.errorLabel.setText('')  # 에러표기창 초기화
+
+        title = self.searchLine.text()
+
+        if title.find('(ARTIST)') != -1:
+            album_title = title.split('(ARTIST)')[0].rstrip()
+
+            try:
+                if self.df_tfidf[self.df_tfidf['album titles'] == album_title]['album titles'].count() == 0:  # 일치 앨범 없을 때
+                    self.errorLabel.setText('일치하는 앨범 또는 아티스트가 없습니다.')
+
+                elif self.df_tfidf[self.df_tfidf['album titles'] == album_title]['album titles'].count() == 1:  # 일치 앨범 1개일 때
+
+                    album_idx = self.df_tfidf[self.df_tfidf['album titles'] == album_title].index[0]
+
+
+                cosine_sim = linear_kernel(self.Tfidf_matrix[album_idx], self.Tfidf_matrix)  # 코사인 유사도
+                recommendation = self.getRecommendation(cosine_sim)  # 추천 함수 실행
+                recommend_df = recommendation.iloc[1:11, 0:2]
+                nums = []
+                contents = []
+                for i in range(8):
+                    num = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 0]
+                    nums.append(num)
+                    title = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 1]
+                    artist = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 2]
+                    contents.append(title + '\n - ' + artist)
+
+                ResultPage(self)
+            except Exception as e:
+                print(e)
+
+
+        else:
+            artist = title
+            artist_album_count = self.df_tfidf[self.df_tfidf['artists'] == artist]['album titles'].count()
+            try:
+                if artist_album_count == 0:
+                    self.errorLabel.setText('일치하는 앨범 또는 아티스트가 없습니다.')
+
+                else:
+                    total_cosine_sim = []  # 각 앨범마다 코사인 유사도를 합친 값 미리 지정
+                    for i in range(artist_album_count):
+                        album_idx = self.df_tfidf[self.df_tfidf['artists'] == artist].index[i]
+                        cosine_sim = linear_kernel(self.Tfidf_matrix[album_idx], self.Tfidf_matrix)
+                        if total_cosine_sim == []:  # 첫 번째는
+                            total_cosine_sim = cosine_sim  # 0번 인덱스 값 대입
+                        else:  # 그 이후는
+                            total_cosine_sim = total_cosine_sim + cosine_sim  # 합치기
+
+                    recommendation = self.getRecommendation(total_cosine_sim)
+                    recommendation = recommendation[recommendation.artists != artist]  # 해당 아티스트의 앨범 제거
+
+                    recommend_df = recommendation.iloc[0:8, 0:2]
+                    nums = []
+                    contents = []
+                    for i in range(8):
+                        num = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 0]
+                        nums.append(num)
+                        title = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 1]
+                        artist = self.df_bugs[self.df_bugs['album titles'] == recommend_df.iloc[i, 0]].iloc[0, 2]
+                        contents.append(title + '\n - ' + artist)
+
+                    ResultPage(self)
+
+            except Exception as e:
+                print(e)
+
+
 
     def getRecommendation(self, cosine_sim):      # 코사인 유사도를 높은 순으로 정렬하여 유사한 앨범의 인덱스 구하는 함수
         simScore = list(enumerate(cosine_sim[0]))
@@ -164,6 +340,13 @@ class ResultPage(QDialog):
 
         self.initUI()
 
+        self.linkbuttons = [self.linkButton_1, self.linkButton_2, self.linkButton_3, self.linkButton_4,
+                       self.linkButton_5, self.linkButton_6, self.linkButton_7, self.linkButton_8]
+
+        for r in range(8):
+            self.linkbuttons[r].clicked.connect(self.gotoURL)
+
+
         self.setWindowTitle("recommend")
         images = [self.image_1, self.image_2, self.image_3, self.image_4,
                   self.image_5, self.image_6, self.image_7, self.image_8]
@@ -180,6 +363,26 @@ class ResultPage(QDialog):
             self.exec_()
         except Exception as e:
             print(e)
+
+
+
+    def gotoURL(self):
+        try:
+            main_url = 'https://music.bugs.co.kr/album/'
+
+            self.object_name = self.sender().objectName()
+
+            for i in range(8):
+                if self.object_name == f'linkButton_{i+1}':
+                    imgnum = i
+
+            url = main_url + str(nums[imgnum])
+            webbrowser.open(url)
+        except Exception as e:
+            print(e)
+
+
+
 
     def initUI(self):
 
